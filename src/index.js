9d3c3937dc91f1b80a7bccc497bb237443a091a8
EdsonRosales97 <erosales@applab.mx>
2023-05-30 13:41:48 -0600

    feat[ExpressStripe]: Router created & server initialized

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import paymentRoutes from "./routes/payment.routes.js";
 
 const app = express();
 
 app.use(express.json())
+
+// Router
+app.use(paymentRoutes);
+
+// Init server
+app.listen(3000);
+console.log("Server on port 3000");
