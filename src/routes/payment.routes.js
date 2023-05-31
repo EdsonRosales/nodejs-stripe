ff1463c762f80f7c6fe956977db3853db65dffde
EdsonRosales97 <erosales@applab.mx>
2023-05-31 12:54:54 -0600

    feat[Routes]: Creation of some routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,4 +2,13 @@ import { Router } from "express";
 
 const router = Router();
 
+// This route is when the user begin with the payment (Route name based on Stripe example)
+router.get('/create-checkout-session', (req, res) => res.send('Send'));
+
+// Payment successful
+router.get('/success', (req, res) => res.send('Success'))
+
+// Payment error
+router.get('/cancel', (req, res) => res.send('Cancel'))
+
 export default router;
