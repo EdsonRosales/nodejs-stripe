b83624db49b102494b48203d18a0dd5f619f5c36
EdsonRosales97 <erosales@applab.mx>
2023-05-31 13:07:28 -0600

    fix[Config]: Controllers folder created and payment controller file

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { createSession } from "../controllers/payment.controller.js";
 
 const router = Router();
 
 // This route is when the user begin with the payment (Route name based on Stripe example)
-router.get('/create-checkout-session', (req, res) => res.send('Send'));
+router.get('/create-checkout-session', createSession);
 
 // Payment successful
 router.get('/success', (req, res) => res.send('Success'))
